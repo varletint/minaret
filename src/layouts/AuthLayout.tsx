38743653ae47b9abb09bb3ad48482060017832b8
varletint <intvarlet@hotmail.com>
2026-02-03 23:47:32 +0100

    auth layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,10 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "@/components/Navbar";
 
-/**
- * Auth layout - minimal layout for login/register pages
- * No footer, centered content
- */
 export function AuthLayout() {
   return (
     <div className='min-h-screen bg-background text-foreground flex flex-col'>
